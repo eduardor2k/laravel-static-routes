6d0055ac6c90d7353503650d12a94a2a72148722
Eduardo Alonso <Eduardo1185>
2021-02-05 00:40:36 +0100

    inject laravel classes

--- src/Commands/StaticRoutesApache2Command.php
+++ src/Commands/StaticRoutesApache2Command.php
@@ -2,19 +2,19 @@
 namespace Eduardor2k\LaravelStaticRoutes\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\View;
 use Illuminate\View\Factory as ViewFactory;
+use Illuminate\Routing\Router;
+use Illuminate\Filesystem\Filesystem;
 
 class StaticRoutesApache2Command extends Command
 {
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'laravel-static-routes:apache-2';
+    protected $signature = 'laravel-static-routes:apache-2 {filename?}';
 
     /**
      * The console command description.
@@ -23,11 +23,19 @@ class StaticRoutesApache2Command extends Command
      */
     protected $description = 'Generate .htaccess file in the public folder';
 
-    public function __construct(ViewFactory $view)
+    private ViewFactory $view;
+
+    private Router $router;
+
+    private Filesystem $filesystem;
+
+    public function __construct(ViewFactory $view, Router $router, Filesystem $filesystem)
     {
         parent::__construct();
 
         $this->view = $view;
+        $this->router = $router;
+        $this->filesystem = $filesystem;
     }
 
     /**
@@ -35,29 +43,35 @@ class StaticRoutesApache2Command extends Command
      */
     public function handle()
     {
-        $content = $this->view->make('laravel-static-routes::apache2.htaccess', ['routes' => $this->build()]);
+        $filePath = public_path(($this->argument('filename') ?? '.htaccess'));
+        $content = $this->view->make('laravel-static-routes::apache2.htaccess', ['routes' => $this->getRoutes()]);
 
-        $filePath = public_path().'/.htaccess';
-        if(File::put($filePath, $content)){
+        if($this->filesystem->put($filePath, $content)){
             $this->info('.htaccess generated correctly in public directory');
             return;
         }
         $this->error(sprintf('Unable to save .htaccess file at %s',$filePath));
     }
 
-    protected function build()
+    protected function getRoutes()
     {
         $data = [];
         /**
          * @var \Illuminate\Routing\Route $route
          */
-        foreach(app()->routes->getRoutes() as $route){
+        foreach($this->router->getRoutes() as $route){
             $data[$this->replace($route->uri())][] = $route->getName();
         }
 
         return $data;
     }
 
+    /**
+     * Replace laravel parameters with regular expressión for apache
+     *
+     * @param string $uri
+     * @return string
+     */
     protected function replace($uri)
     {
         $regex = '/\{[a-z:A-Z0-9_\?]+\}/';
